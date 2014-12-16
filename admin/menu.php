0d3309e30efdb7c3cff089ba4c0394f988a11a07
MKurshumov <kur6uma666@users.noreply.github.com>
2014-12-16 19:11:39 +0200

    modified menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -1,9 +1,16 @@
-<h1>Blog</h1>
-<ul id='adminmenu'>
-	<li><a href='index.php'>Blog</a></li>
-	<li><a href='users.php'>Users</a></li>
-	<li><a href="../" target="_blank">View Website</a></li>
-	<li><a href='logout.php'>Logout</a></li>
-</ul>
+<div class="panel panel-default menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">Admin Panel</h3>
+    </div>
+    <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked" id="adminmenu">
+            <li role="presentation" class="opts"><a href='index.php'>Blog</a></li>
+            <li role="presentation" class="opts"><a href='users.php'>Users</a></li>
+            <li role="presentation" class="opts"><a href="../" target="_blank">View Website</a></li>
+            <li role="presentation" class="opts">
+                <a href='logout.php'>Logout <span class="glyphicon glyphicon-log-out"></span></a>
+            </li>
+        </ul>
+    </div>
+</div>
 <div class='clear'></div>
-<hr />
